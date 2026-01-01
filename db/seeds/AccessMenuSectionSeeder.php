54300c83930bb3b74e2366b65cffb21f1472550f
kuyabatok <maulana858@gmail.com>
2026-01-01 22:56:35 +0700

    update phinx & adding arsip menu to seeder

--- db/seeds/AccessMenuSectionSeeder.php
+++ db/seeds/AccessMenuSectionSeeder.php
@@ -4,128 +4,133 @@ use Phinx\Seed\AbstractSeed;
 
 class AccessMenuSectionSeeder extends AbstractSeed
 {
-    /**
-     * Run Method.
-     *
-     * Write your database seeder using this method.
-     *
-     * More information on writing seeders is available here:
-     * https://book.cakephp.org/phinx/0/en/seeding.html
-     */
-    public function run(): void
-    {
-        $table = $this->table('access_menu_section');
-        $aksesAdmin = [
-            [
-                'group_id' => 1,
-                'menu_section_id' => 1,
-            ],
-            [
-                'group_id' => 1,
-                'menu_section_id' => 2,
-            ],
-            [
-                'group_id' => 1,
-                'menu_section_id' => 3,
-            ],
-            [
-                'group_id' => 1,
-                'menu_section_id' => 4,
-            ],
-            [
-                'group_id' => 1,
-                'menu_section_id' => 5,
-            ],
-            [
-                'group_id' => 1,
-                'menu_section_id' => 6,
-            ],
-            [
-                'group_id' => 1,
-                'menu_section_id' => 7,
-            ],
-            [
-                'group_id' => 1,
-                'menu_section_id' => 8,
-            ],
-        ];
+	/**
+	 * Run Method.
+	 *
+	 * Write your database seeder using this method.
+	 *
+	 * More information on writing seeders is available here:
+	 * https://book.cakephp.org/phinx/0/en/seeding.html
+	 */
+	public function run(): void
+	{
+		$table = $this->table("access_menu_section");
+		$aksesAdmin = [
+			[
+				"group_id" => 1,
+				"menu_section_id" => 1,
+			],
+			[
+				"group_id" => 1,
+				"menu_section_id" => 2,
+			],
+			[
+				"group_id" => 1,
+				"menu_section_id" => 3,
+			],
+			[
+				"group_id" => 1,
+				"menu_section_id" => 4,
+			],
+			[
+				"group_id" => 1,
+				"menu_section_id" => 5,
+			],
+			[
+				"group_id" => 1,
+				"menu_section_id" => 6,
+			],
+			[
+				"group_id" => 1,
+				"menu_section_id" => 7,
+			],
+			[
+				"group_id" => 1,
+				"menu_section_id" => 8,
+			],
+			[
+				"group_id" => 1,
+				"menu_section_id" => 9,
+			],
+		];
 
-        $aksesPanmudHukum = [
-            [
-                'group_id' => 430,
-                'menu_section_id' => 1,
-            ],
-            [
-                'group_id' => 430,
-                'menu_section_id' => 2,
-            ],
-            [
-                'group_id' => 430,
-                'menu_section_id' => 3,
-            ],
-            [
-                'group_id' => 430,
-                'menu_section_id' => 5,
-            ],
-            [
-                'group_id' => 430,
-                'menu_section_id' => 8,
-            ],
-        ];
+		$aksesPanmudHukum = [
+			[
+				"group_id" => 430,
+				"menu_section_id" => 1,
+			],
+			[
+				"group_id" => 430,
+				"menu_section_id" => 2,
+			],
+			[
+				"group_id" => 430,
+				"menu_section_id" => 3,
+			],
+			[
+				"group_id" => 430,
+				"menu_section_id" => 5,
+			],
+			[
+				"group_id" => 430,
+				"menu_section_id" => 8,
+			],
+		];
 
-        $aksesPanmudGugatan = [
-            [
-                'group_id' => 1000,
-                'menu_section_id' => 1,
-            ],
-            [
-                'group_id' => 1000,
-                'menu_section_id' => 4,
-            ],
-            [
-                'group_id' => 1000,
-                'menu_section_id' => 6,
-            ],
-        ];
+		$aksesPanmudGugatan = [
+			[
+				"group_id" => 1000,
+				"menu_section_id" => 1,
+			],
+			[
+				"group_id" => 1000,
+				"menu_section_id" => 4,
+			],
+			[
+				"group_id" => 1000,
+				"menu_section_id" => 6,
+			],
+		];
 
-        $aksesPanmudPermohonan = [
-            [
-                'group_id' => 1010,
-                'menu_section_id' => 1,
-            ],
-            [
-                'group_id' => 1010,
-                'menu_section_id' => 7,
-            ],
-        ];
+		$aksesPanmudPermohonan = [
+			[
+				"group_id" => 1010,
+				"menu_section_id" => 1,
+			],
+			[
+				"group_id" => 1010,
+				"menu_section_id" => 7,
+			],
+		];
 
-        $aksesMeja3 = [
-            [
-                'group_id' => 1003,
-                'menu_section_id' => 1,
-            ],
-            [
-                'group_id' => 1003,
-                'menu_section_id' => 2,
-            ],
-            [
-                'group_id' => 1003,
-                'menu_section_id' => 3,
-            ],
-            [
-                'group_id' => 1003,
-                'menu_section_id' => 4,
-            ]
-        ];
+		$aksesMeja3 = [
+			[
+				"group_id" => 1003,
+				"menu_section_id" => 1,
+			],
+			[
+				"group_id" => 1003,
+				"menu_section_id" => 2,
+			],
+			[
+				"group_id" => 1003,
+				"menu_section_id" => 3,
+			],
+			[
+				"group_id" => 1003,
+				"menu_section_id" => 4,
+			],
+		];
 
-
-        $table->insert(array_merge(
-            $aksesAdmin,
-            $aksesPanmudHukum,
-            $aksesPanmudGugatan,
-            $aksesPanmudPermohonan,
-            $aksesMeja3
-        ));
-        $table->saveData();
-    }
+		$table->insert(
+			array_merge(
+				$aksesAdmin,
+				$aksesPanmudHukum,
+				$aksesPanmudGugatan,
+				$aksesPanmudPermohonan,
+				$aksesMeja3,
+			),
+		);
+		$table->saveData();
+	}
 }
--- db/seeds/BulanSeeder.php
+++ db/seeds/BulanSeeder.php
@@ -1,34 +1,83 @@
 <?php
 
-
 use Phinx\Seed\AbstractSeed;
 
 class BulanSeeder extends AbstractSeed
 {
-    /**
-     * Run Method.
-     *
-     * Write your database seeder using this method.
-     *
-     * More information on writing seeders is available here:
-     * https://book.cakephp.org/phinx/0/en/seeding.html
-     */
-    public function run()
-    {
-        $table = $this->table('bulan');
-        $table->insert([
-            ['nama' => 'Januari', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' => 'Februari', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' => 'Maret', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' => 'April', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' => 'Mei', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' => 'Juni', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' => 'Juli', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' => 'Agustus', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' => 'September', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' => 'Oktober', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' =>  'November', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['nama' => 'Desember', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-        ])->saveData();
-    }
+	/**
+	 * Run Method.
+	 *
+	 * Write your database seeder using this method.
+	 *
+	 * More information on writing seeders is available here:
+	 * https://book.cakephp.org/phinx/0/en/seeding.html
+	 */
+	public function run(): void
+	{
+		$table = $this->table("bulan");
+		$table
+			->insert([
+				[
+					"nama" => "Januari",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "Februari",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "Maret",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "April",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "Mei",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "Juni",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "Juli",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "Agustus",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "September",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "Oktober",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "November",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+				[
+					"nama" => "Desember",
+					"created_at" => date("Y-m-d H:i:s"),
+					"updated_at" => date("Y-m-d H:i:s"),
+				],
+			])
+			->saveData();
+	}
 }
